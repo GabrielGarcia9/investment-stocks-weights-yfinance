1ca94296cda88a11966a7c2b313c34f01570e38f
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-10-31 08:20:46 -0500

    Agregar funcionalidad de cambio de color y aplicar en los mensajes de descripción, link y excepciones

--- main.py
+++ main.py
@@ -13,25 +13,34 @@ def print_table(df, theme=Themes.OCEAN):
     table.add_rows(df.values.tolist())
     print(table)
 
-# main.py (parte donde descargas los datos)
+def print_with_color_and_format(text, color_code, bold=False, underline=False):
+    start_code = "\033["
+    if bold:
+        start_code += "1;"
+    if underline:
+        start_code += "4;"
+    start_code += f"{color_code}m"
+    end_code = "\033[0m"
+    print(f"{start_code}{text}{end_code}") # rojo:31, verde:32, amarillo:33, azul:34
+
+
+# main.py 
 def main():
     # Introducción y descripción de variables financieras
-    print("""
-    --- Bienvenido al Informe de Análisis de Inversión ---
+    print_with_color_and_format("--- Bienvenido al Informe de Análisis de Inversión ---", 32, bold=True)
+    print_with_color_and_format("""
+Este programa permite realizar un análisis de acciones bursátiles mediante los siguientes conceptos:
     
-    Este programa permite realizar un análisis de acciones bursátiles mediante los siguientes conceptos:
-    
-    1) Rendimiento Diario: Cambio porcentual diario en el precio de las acciones, indicando la ganancia o pérdida relativa.
-    2) Volatilidad: Mide la variabilidad del precio de las acciones a lo largo del tiempo.
-    3) Peso Óptimo: Distribución óptima de la inversión en las acciones seleccionadas, basada en el rendimiento y la volatilidad.
+1) Rendimiento Diario: Cambio porcentual diario en el precio de las acciones, indicando la ganancia o pérdida relativa.
+2) Volatilidad: Mide la variabilidad del precio de las acciones a lo largo del tiempo.
+3) Peso Óptimo: Distribución óptima de la inversión en las acciones seleccionadas, basada en el rendimiento y la volatilidad.
 
-    A continuación, se le pedirá que ingrese los símbolos de las acciones y el rango de fechas para el análisis.
-    
-    Puede consultar tickers en: 
-    Yahoo Finance (https://finance.yahoo.com/lookup/) o 
-    Nasdaq Symbol Directory (https://www.nasdaq.com/market-activity/stocks/screener)
+A continuación, se le pedirá que ingrese los símbolos de las acciones y el rango de fechas para el análisis.
+Puede consultar tickers en: """, 32)
+    print_with_color_and_format("Yahoo Finance:", 32); print_with_color_and_format("https://finance.yahoo.com/lookup/", 34) 
+    print_with_color_and_format("Nasdaq Symbol Directory:",32); print_with_color_and_format("https://www.nasdaq.com/market-activity/stocks/screener", 34)
 
-    """)
+    
 
     try:
         tickers = input("Ingrese los símbolos (es decír más de uno) de los tickers separados por comas (ejemplo: MSFT, TSLA, NVDA): ").split(",")
@@ -45,13 +54,11 @@ def main():
             data = downloader.downloader_data(tickers, date_start, date_end)
             data = analyse_data.add_performance(data, tickers)
             
-    
-                
             if data.empty:
                 raise ValueError("No se pudieron descargar datos para los tickers especificados.")
                 
         except Exception as e:
-            print(f"Error al descargar los datos: {e}")
+            print_with_color_and_format(f"Error al descargar los datos: {e}", 31)
             return  # Finalizar si la descarga falla
 
         fin = False
@@ -65,7 +72,7 @@ def main():
             try:
                 opc = int(input("Ingresar opción: "))
             except ValueError:
-                print("Por favor, ingrese un número válido.")
+                print_with_color_and_format("Por favor, ingrese un número válido.", 31)
                 continue  # Volver a mostrar el menú si la entrada no es un número
 
             if opc == 1:
@@ -77,7 +84,7 @@ def main():
                     stats.columns = ["Ticker"] + stats.columns.tolist()[1:]
                     print_table(stats)
                 except Exception as e:
-                    print(f"Error al mostrar el análisis descriptivo: {e}")
+                    print_with_color_and_format(f"Error al mostrar el análisis descriptivo: {e}", 31)
 
             elif opc == 2:
                 print("""Escoja el tipo de gráfico que desea visualizar:
@@ -92,9 +99,9 @@ def main():
                     else:
                         print("Opción no válida.")
                 except ValueError:
-                    print("Por favor, ingrese un número válido.")
+                    print_with_color_and_format("Por favor, ingrese un número válido.", 31)
                 except Exception as e:
-                    print(f"Error al generar el gráfico: {e}")
+                    print_with_color_and_format(f"Error al generar el gráfico: {e}", 31)
 
             elif opc == 3:
                 try:
@@ -104,7 +111,7 @@ def main():
                     weights_df = pd.DataFrame(weights.items(), columns=["Ticker", "Peso Óptimo"])
                     print_table(weights_df)
                 except Exception as e:
-                    print(f"Error al calcular el peso óptimo: {e}")
+                    print_with_color_and_format(f"Error al calcular el peso óptimo: {e}", 31)
 
             elif opc == 4:
 
@@ -117,21 +124,21 @@ def main():
                     try:
                         generar_informe(data, tickers, calculate_weight.calculate_weight(mu = calculate_weight.calculate_mu_sigma(data, tickers)[0], sigma=calculate_weight.calculate_mu_sigma(data, tickers)[1]))
                     except Exception as e:
-                        print(f"Error al generar el informe: {e}")
+                        print_with_color_and_format(f"Error al generar el informe: {e}", 31)
                 elif opc == 2:
                     try:
                         generar_excel(data)
                     except Exception as e:
-                        print(f"Error al generar el excel: {e}")
+                        print_with_color_and_format(f"Error al generar el excel: {e}", 31)
 
             elif opc == 5:
                 fin = True
                 print("Saliendo del programa... ¡Hasta luego!")
             else:
-                print("Opción no válida. Intente nuevamente.")
+                print_with_color_and_format("Opción no válida. Intente nuevamente.", 31)
 
     except Exception as e:
-        print(f"Se ha producido un error inesperado: {e}")
+        print_with_color_and_format(f"Se ha producido un error inesperado: {e}", 31)
 
 if __name__ == "__main__":
     main()
